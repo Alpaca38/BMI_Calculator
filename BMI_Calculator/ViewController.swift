04e6f9e4e6b1b691559ca7ba8a2784e268ffed9b
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-05-21 18:28:40 +0900

    계산 로직 적용, 예외처리 적용

--- BMI_Calculator/ViewController.swift
+++ BMI_Calculator/ViewController.swift
@@ -22,43 +22,94 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configureLabel(label: titleLabel, text: "BMI Calculator", font: .boldSystemFont(ofSize: 24))
-        configureLabel(label: messageLabel, text: "당신의 BMI 지수를\n알려드릴게요.", font: .systemFont(ofSize: 14))
+        configureLabel(titleLabel, text: "BMI Calculator", font: .boldSystemFont(ofSize: 24))
+        configureLabel(messageLabel, text: "당신의 BMI 지수를\n알려드릴게요.", font: .systemFont(ofSize: 14))
         
         mainImageView.image = .image
         
-        configureLabel(label: askHeightLabel, text: "키가 어떻게 되시나요?", font: .systemFont(ofSize: 15))
-        configureLabel(label: askWeightLabel, text: "몸무게가 어떻게 되시나요?", font: .systemFont(ofSize: 15))
+        configureLabel(askHeightLabel, text: "키가 어떻게 되시나요?", font: .systemFont(ofSize: 15))
+        configureLabel(askWeightLabel, text: "몸무게가 어떻게 되시나요?", font: .systemFont(ofSize: 15))
         
-        configureTextField()
+        configureTextField(heightTextField, placeholder: "m(미터) 단위로 숫자만 입력해주세요")
+        configureTextField(weightTextField, placeholder: "kg 단위로 숫자만 입력해주세요")
         
-        configureButton(button: randomButton, text: "랜덤으로 BMI 계산하기", tintColor: .brown, backgroundColor: .systemBackground, size: 12)
-        configureButton(button: calculateButton, text: "결과 확인", tintColor: .white, backgroundColor: .systemPurple, size: 20)
+        configureButton(randomButton, text: "랜덤으로 BMI 계산하기", tintColor: .brown, backgroundColor: .systemBackground, size: 12)
+        configureButton(calculateButton, text: "결과 확인", tintColor: .white, backgroundColor: .systemPurple, size: 20)
     }
     
     @IBAction func randomButtonTapped(_ sender: UIButton) {
+        var randomHeight = Float.random(in: 1.5...2.5)
+        var randomWeight = Float.random(in: 40...200)
+        
+        showResultAlert(bmi: calculateBMI(height: randomHeight, weight: randomWeight))
         
     }
     
     @IBAction func calculateButtonTapped(_ sender: UIButton) {
         
+        if let height = Float(heightTextField.text!), let weight = Float(weightTextField.text!) {
+            showResultAlert(bmi: calculateBMI(height: height, weight: weight))
+        } else if heightTextField.text == "" && weightTextField.text == "" {
+            showBlankAlert()
+        } else {
+            showAlert()
+        }
     }
     
-    func configureLabel(label: UILabel, text: String, font: UIFont) {
+    func calculateBMI(height: Float, weight: Float) -> Float {
+        if height > 10 {
+            let mHeight = height / 100
+            let result = weight / mHeight / mHeight
+            
+            return result
+        } else {
+            let result = weight / height / height
+            
+            return result
+        }
+    }
+    
+    func showAlert() {
+        let alert = UIAlertController(title: "잘못된 값을 입력하셨습니다.", message: "숫자만 입력해주세요.", preferredStyle: .alert)
+        let check = UIAlertAction(title: "확인", style: .default)
+        
+        alert.addAction(check)
+        
+        present(alert, animated: true)
+    }
+    
+    func showBlankAlert() {
+        let alert = UIAlertController(title: "아무것도 입력하지 않았습니다.", message: "키와 몸무게를 입력해주세요.", preferredStyle: .alert)
+        let check = UIAlertAction(title: "확인", style: .default)
+        
+        alert.addAction(check)
+        
+        present(alert, animated: true)
+    }
+    
+    func showResultAlert(bmi: Float) {
+        let alert = UIAlertController(title: "BMI 계산 결과입니다.", message: "\(bmi)", preferredStyle: .alert)
+        let check = UIAlertAction(title: "확인", style: .default)
+        
+        alert.addAction(check)
+        
+        present(alert, animated: true)
+    }
+    
+    func configureLabel(_ label: UILabel, text: String, font: UIFont) {
         label.text = text
         label.font = font
         label.numberOfLines = 0
     }
     
-    func configureTextField() {
-        let textfields = [heightTextField, weightTextField]
+    func configureTextField(_ textField: UITextField, placeholder: String) {
         
-        textfields.forEach { $0?.layer.cornerRadius = 10
-            $0?.layer.borderWidth = 1
-        }
+        textField.layer.cornerRadius = 10
+        textField.layer.borderWidth = 1
+        textField.placeholder = placeholder
     }
     
-    func configureButton(button: UIButton, text: String, tintColor: UIColor, backgroundColor: UIColor?, size: CGFloat) {
+    func configureButton(_ button: UIButton, text: String, tintColor: UIColor, backgroundColor: UIColor?, size: CGFloat) {
         button.setTitle(text, for: .normal)
         button.layer.cornerRadius = 10
         button.tintColor = tintColor
